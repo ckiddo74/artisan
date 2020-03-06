dc0498eb16d1337d0526471299f88227fd3ce04d
Gabriel Figueiredo <gabriel.figueiredo@imperial.ac.uk>
2020-03-06 20:24:46 +0000

    added Expr::stmt() method which returns the statement associated with any expression

--- artisan/rose/src/entity/defs/Expr.hpp
+++ artisan/rose/src/entity/defs/Expr.hpp
@@ -5,8 +5,7 @@
 ENTITY_SPEC_BEGIN(Expr, "expression", SgExpression, NodeSrc, node, obj, entity, sg_type) {
     bind_method(obj, "type", "returns expression type", type);
     bind_method(obj, "is_val", "returns whether expression represents a constant value", is_val);
-
-
+    bind_method(obj, "stmt", "returns (parent) statement associated with this expression", stmt);
 }  
 
 static py::object type(py::object self) {
@@ -17,6 +16,15 @@ static py::object type(py::object self) {
 static bool is_val(py::object self) {
     SgExpression *node = (SgExpression *) to_sgnode(self);   
     return (bool) isSgValueExp(node);
+}
+
+static py::object stmt(py::object self) {
+    SgExpression *node = (SgExpression *) to_sgnode(self);   
+    SgNode *parent = node->get_parent();
+    while (parent && !isSgStatement(parent)) {
+        parent = parent->get_parent();
+    }
+    return create_rose_node(parent);    
 }
   
 ENTITY_SPEC_END
--- artisan/rose/src/version.hpp
+++ artisan/rose/src/version.hpp
@@ -1,2 +1,2 @@
-#define ARTISAN_VERSION "2003-05"
-#define ARTISAN_DESC "support for 'new' and 'delete'"
+#define ARTISAN_VERSION "2003-06"
+#define ARTISAN_DESC "added Expr::stmt() method which returns the statement associated with any expression"
